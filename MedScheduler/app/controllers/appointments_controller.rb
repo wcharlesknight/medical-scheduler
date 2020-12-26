1babb17b627cae03fd2bcd4c49ba9313fa7af9f9
Drothschild89 <Drothschild89@gmail.com>
2020-12-26 14:31:33 -0800

    Datetime fixed

--- MedScheduler/app/controllers/appointments_controller.rb
+++ MedScheduler/app/controllers/appointments_controller.rb
@@ -22,7 +22,7 @@ class AppointmentsController < ApplicationController
     private
 
     def appt_params
-        params.require(:appointment).permit(:datetime, :doctor_id, :patient_id)
+        params.require(:appointment).permit(:datetime_id, :doctor_id, :patient_id)
     end
 
 
