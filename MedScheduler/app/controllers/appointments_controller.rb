dd19debbd70f7bd9a004af03e0e26d4c30b71b21
Drothschild89 <Drothschild89@gmail.com>
2021-01-05 11:18:39 -0800

    Added appointment uniqueness + error message

--- MedScheduler/app/controllers/appointments_controller.rb
+++ MedScheduler/app/controllers/appointments_controller.rb
@@ -18,9 +18,12 @@ class AppointmentsController < ApplicationController
     def create
         @appointment = Appointment.create(appt_params) 
         @doctor = Doctor.find(@appointment.doctor_id)
-        # @doctor.appointments << @appointment
-        # @doctor.patients << Patient.find(@appointment.patient_id)
+        if @appointment.valid?
         redirect_to doctor_path(@appointment.doctor_id)
+        else
+            flash[:errors] = @appointment.errors.full_messages
+            redirect_to new_appointment_path
+        end
     end
 
     def destroy
