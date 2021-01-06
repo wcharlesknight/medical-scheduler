12f2c2d31679fec96f4a95580e7195c01738bf04
Drothschild89 <Drothschild89@gmail.com>
2021-01-06 10:34:53 -0800

    Patient index page done

--- MedScheduler/app/controllers/specialties_controller.rb
+++ MedScheduler/app/controllers/specialties_controller.rb
@@ -2,11 +2,11 @@ class SpecialtiesController < ApplicationController
     def index
         @specialties = Specialty.all
         @doctors = Doctor.all
-        @highest = Specialty.highest_rated
+        @highest = Specialty.highest_rating
     end
 
     def show
         @specialty = Specialty.find(params[:id])
     end
-    
+
 end
